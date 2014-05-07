8663e2d87b21f85783d993dba4ba1ee6461054d3
Garrett Houghton <ghought@gmail.com>
2014-05-06 21:58:40 -0400

    Initial Commit
    First Commit

--- Funky Unit Converter/GHViewController.m
+++ Funky Unit Converter/GHViewController.m
@@ -17,7 +17,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello World");
+	int x = 5;
+    int y = 20;
+    int z = -2;
+    
+    // operators include: + - * /
+    
+    int additionAnswer = x + y;
+    int multiplicationAnswer = y * z;
+    
+    
+    float heightOfEverestBaseCamp = 16900.3;
+    float heightOfEverest = 29029;
+    
+    float distanceToTravel = heightOfEverest - heightOfEverestBaseCamp;
+    distanceToTravel = distanceToTravel - 1000;
 }
 
 - (void)didReceiveMemoryWarning
